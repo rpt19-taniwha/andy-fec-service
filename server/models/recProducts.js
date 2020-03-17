67d596a28c71d59c75dbe1030684a67af53d5a1e
Andy Gala <livelifebrands@gmail.com>
2020-03-16 19:33:53 -0700

    clean up code

--- server/models/recProducts.js
+++ server/models/recProducts.js
@@ -1,17 +1,58 @@
 const mongoose = require("mongoose");
-// const { recData } = require("../data/recData");
 
 const Schema = mongoose.Schema;
 
 const productSchema = new Schema({
   productNumber: {
+    type: Number,
+    required: true
+  },
+  productName: {
     type: String,
     required: true
   },
-  recProducts: {
+  price: {
+    type: String,
+    required: true
+  },
+  picture: {
+    type: String,
+    required: true
+  },
+  sellerName: {
+    type: String,
+    required: true
+  },
+  shipping: {
+    type: Boolean,
+    required: false
+  },
+  catagory: {
+    type: String,
+    required: false
+  },
+  metaData: {
     type: [String],
     required: false
+  },
+  recProducts: {
+    type: [Object],
+    required: true
   }
 });
 
 module.exports = mongoose.model("Product", productSchema);
+/*
+Prduct Schema
+{
+	productNumber: Number,
+	productName: String,
+	productPrice: String,
+	productPicture: String(Url),
+	sellerName: String,
+  shipping: Boolean,
+  productCatagory: String,
+	metaData: [String],
+	recProducts: [Product]
+}
+*/
