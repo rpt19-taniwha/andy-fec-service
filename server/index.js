76221a6b6461d6bc95f3eaa5636f71e779c84890
Andy Gala <livelifebrands@gmail.com>
2020-03-22 14:22:30 -0700

    Setup eslint and prettier

--- server/index.js
+++ server/index.js
@@ -1,24 +1,23 @@
-const express = require("express");
-const path = require("path");
-const bodyParser = require("body-parser");
+const express = require('express');
+const path = require('path');
 
-const mongoose = require("mongoose");
-const dbUrl = require("./database-mongo/database.config");
-const Product = require("./models/recProducts");
+const bodyParser = require('body-parser');
+
+const mongoose = require('mongoose');
+const dbUrl = require('./database-mongo/database.config');
+const Product = require('./models/recProducts');
 
 const app = express();
-let port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
-//=========================================
-//API Endpoints
-//=========================================
-app.get("/youmayalsolike", (req, res) => {
-  console.log(req.query);
+//  API Endpoints
+
+app.get('/youmayalsolike', (req, res) => {
   Product.findOne(req.query)
     .then(data => {
       res.send(data);
@@ -28,10 +27,12 @@ app.get("/youmayalsolike", (req, res) => {
     });
 });
 
-//Start server and db
+//  Start server and db
 mongoose
   .connect(dbUrl, { useUnifiedTopology: true, useNewUrlParser: true })
-  .then(client => {
-    app.listen(port, () => console.log(`Listening on ${port}...`));
+  .then(() => {
+    app.listen(port);
   })
-  .catch(err => console.log(err));
+  .catch(err => {
+    throw err;
+  });
