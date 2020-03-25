b3fb8d34d440528437a514bdd942cf3815af9d68
Andy Gala <livelifebrands@gmail.com>
2020-03-25 12:37:02 -0700

    Refactor code

--- server/controllers/app.js
+++ server/controllers/app.js
@@ -14,10 +14,10 @@ app.use(express.static(path.join(__dirname, '../client/dist')));
 
 //  API Endpoints
 
-app.get('/youmayalsolike/:productId', (req, res) => {
-  const productId = parseInt(req.params.productId, 10);
+app.get('/youmayalsolike/:productNumber', (req, res) => {
+  const productNumber = parseInt(req.params.productNumber, 10);
 
-  Product.findOne({ productNumber: productId })
+  Product.findOne({ productNumber })
     .then((data) => {
       res.send(data);
     })
