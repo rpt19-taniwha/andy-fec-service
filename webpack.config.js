0215e3af41ebf229d155b895c9d354490e5d69fa
Andy Gala <livelifebrands@gmail.com>
2020-03-25 15:09:31 -0700

    Add htmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 let SRC_DIR = path.join(__dirname, '/client/src');
 let DIST_DIR = path.join(__dirname, '/client/dist');
 
 module.exports = {
+  mode: 'development',
   entry: `${SRC_DIR}/index.jsx`,
   output: {
     filename: 'bundle.js',
@@ -13,10 +15,10 @@ module.exports = {
     rules: [
       {
         test: /\.jsx?/,
-        include: SRC_DIR,
-        loader: "babel-loader",
+        exclude: /node_modules/,
+        loader: 'babel-loader',
         query: {
-          presets: ["@babel/preset-react"]
+          presets: ['@babel/preset-react', '@babel/preset-env']
         }
       },
       {
@@ -24,5 +26,10 @@ module.exports = {
         use: ['style-loader', 'css-loader']
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './client/src/index.html'
+    })
+  ]
 };
