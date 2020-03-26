83fda73626bfb7742fa923ac6a038805677a9feb
Andy Gala <livelifebrands@gmail.com>
2020-03-25 20:38:35 -0700

    Complete client test setup

---
+++ __test__/client.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Enzyme from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+import App from '../client/src/components/App.jsx';
+
+Enzyme.configure({
+  adapter: new EnzymeAdapter(),
+  disableLifecycleMethods: true
+});
+
+describe('Render App component', () => {
+  it('Should include 1 div with "Hello world!!!" text', () => {
+    const wrapper = shallow(<App />);
+
+    console.log(wrapper);
+    expect(wrapper.text()).toBe('Hello world!!!');
+  });
+});
