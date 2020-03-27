4de670d7d5047fd5f43f91f48838b8229df225c5
Andy Gala <livelifebrands@gmail.com>
2020-03-27 13:10:36 -0700

    Render data from db to client

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,18 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      ProductsData = []
+      productsData: []
     };
   }
 
+  componentDidMount() {
+    const productNum = document.location.pathname.split('/')[2];
 
+    axios(`http://localhost:8080/youmayalsolike/549504785`).then(
+      (serverData) => {
+        this.setState({
+          productsData: serverData.data
+        });
+      }
+    );
+  }
 
   render() {
-    return <div>Hello world!!!</div>;
+    return <div>{this.state.productsData.productName}</div>;
   }
 }
 
