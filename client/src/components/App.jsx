0a936b62f21fafd530f1006c05afa570ca1dcae4
Andy Gala <livelifebrands@gmail.com>
2020-04-09 19:36:44 -0700

    Fix css

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -34,7 +34,7 @@ class App extends React.Component {
         <div className="inner-wrapper">
           <div className="title">
             <div className="title-text">You may also like</div>
-            <div className="shop-more">Shop more similar items <i className='fas fa-arrow-right arrow'></i></div>
+            <div className="shop-more">Shop more similar items <div className="arrow">&#8680;</div></div>
           </div>
           <div className="products">
             <Product data={this.getProduct(0)}></Product>
--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -13,20 +13,20 @@ const Product = (props) => {
 
   // Temp helper func to make sure if lorem picsum link is broken we still get a image to render
   // Need to eventually get image url from image service
-  const imageExists = (url) => {
-    let newUrl = `https://i.picsum.photos/id/${Math.floor(Math.random() * 300 + 200)}/220/165.jpg`
-    let image = new Image();
-    image.src = url;
-    if (image.height === 0) {
-      return newUrl;
-    }
-    return url;
-  }
-  const image = imageExists(picture);
+  // const imageExists = (url) => {
+  //   let newUrl = `https://i.picsum.photos/id/${Math.floor(Math.random() * 300 + 200)}/220/165.jpg`
+  //   let image = new Image();
+  //   image.src = url;
+  //   if (image.height === 0) {
+  //     return newUrl;
+  //   }
+  //   return url;
+  // }
+  // const image = imageExists(picture);
 
   return (
     <div className="cards">
-      <img className="rec-product-pic" src={image} alt="picture"></img>
+      <img className="rec-product-pic" src={picture} alt="picture"></img>
       <div className="content">
         <p>{productName}</p>
         <p className="seller-name">{sellerName}</p>
